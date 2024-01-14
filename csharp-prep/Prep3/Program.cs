41a1f93a59b4d3f79d9c0c074e9c434ad2541656
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-01-14 15:26:25 -0300

    step 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("3. C# Prep 3—Loops");
+
+        Console.Write("Choose a number:  ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        Console.Write("Guess the number:  ");
+        string userInput2 = Console.ReadLine();
+        int guessNumber = int.Parse(userInput2);
+
+        if (guessNumber > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guessNumber < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guessNumber == number)
+        {
+            Console.WriteLine("Correct!");
+        }
     }
 }
