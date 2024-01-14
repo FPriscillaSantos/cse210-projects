ada8e6cda0bcc7779a12740f4ce4763694e27e5f
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-01-14 15:41:48 -0300

    complet!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,25 +7,28 @@ class Program
         Console.WriteLine("Hello Prep3 World!");
         Console.WriteLine("3. C# Prep 3—Loops");
 
-        Console.Write("What is the magic number? ");
-        string userInput = Console.ReadLine();
-        int number = int.Parse(userInput);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
 
-        Console.Write("What is your guess? ");
-        string userInput2 = Console.ReadLine();
-        int guessNumber = int.Parse(userInput2);
+        int guessNumber;
+        
+        do
+        {            
+            Console.Write("What is your guess? ");
+            string userInput2 = Console.ReadLine();
+            guessNumber = int.Parse(userInput2);
 
-        if (guessNumber > number)
-        {
-            Console.WriteLine("Lower");
-        }
-        else if (guessNumber < number)
-        {
-            Console.WriteLine("Higher");
-        }
-        else if (guessNumber == number)
-        {
-            Console.WriteLine("You guessed it!");
-        }
+            if (guessNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guessNumber != number);
+        
+        Console.WriteLine("You guessed it!");
+        
     }
 }
