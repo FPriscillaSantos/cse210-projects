d26bceea0d3cc87ad0d69898d24d613ebde77600
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-01-15 19:13:25 -0300

    prep 4  done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("4. C# Prep 4—Lists");
+
+        List<int> numbers = new List<int>();
+        int number;
+        int sum = 0;
+        int lengh = 0;
+        float average = 0;
+        int largest = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        do
+        {
+            Console.Write("Enter number: ");
+            string numberChosen = Console.ReadLine();
+            number = int.Parse(numberChosen);
+            numbers.Add(number);
+        } while (number != 0);
+
+        foreach (int item in numbers)
+        {
+        //    Console.WriteLine(item);
+            lengh = lengh + 1;
+            sum = sum + item;  
+            if (item > largest)
+            {
+                largest = item;
+            };
+        }
+
+        lengh = lengh - 1;
+        average = (float)sum / lengh;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average.ToString("F15")}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
