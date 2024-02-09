8948668df84e438a56e905938b4a1732da012129
FPriscillaSantos <fabiana.priscilla.santos@outlook.com>
2024-02-09 17:29:51 -0300

    menu completo

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -10,15 +10,16 @@ class Program
         Console.WriteLine("");
         Console.WriteLine("Menu Options: \r\n  1. Create New Goal \r\n  2. List Goals \r\n  3. Save Goals \r\n  4. Load Goals \r\n  5. Record Event \r\n  6. Quit");
         Console.Write("Select a choice from the menu: ");
-        // read line
+        string menuChoice = Console.ReadLine();
         Console.WriteLine("");
         Console.Write("The types of Goals are: \r\n  1. Simple Goal \r\n  2. Eternal Goal \r\n  3. Checklist Goal \r\nWhich type of goal would you like to create? ");
-        // read line
+        string typeGoal = Console.ReadLine();
         Console.Write("What is the name of your goal? ");
-        // read line
+        string shortName = Console.ReadLine();
         Console.Write("What is a short description of it? ");
-        // read line
+        string description = Console.ReadLine();
         Console.Write("What is the amount of point associated with this goal? ");
-        // read line
+        string _amountPoints = Console.ReadLine();
+        int amountPoints = int.Parse(_amountPoints);
     }
 }
